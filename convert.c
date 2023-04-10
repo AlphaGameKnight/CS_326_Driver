8bee08e7712177f8bea941d6ba926e9bf23c520a
AlphaGameKnight <alphagameknight1517@hotmail.com>
2023-04-10 15:51:49 -0500

    Added convert.c

---
+++ convert.c
@@ -0,0 +1,36 @@
+/**********************************************************************/
+/*                                                                    */
+/* Program Name: scheduler - Simulate an operating system scheduler   */
+/* Author:       William S. Wu                                        */
+/* Installation: Pensacola Christian College, Pensacola, Florida      */
+/* Course:       CS326, Operating Systems                             */
+/* Date Written: April 9, 2023                                        */
+/*                                                                    */
+/**********************************************************************/
+
+/**********************************************************************/
+/*                                                                    */
+/* I pledge this assignment is my own first time work.                */
+/* I pledge I did not copy or try to copy work from the Internet.     */
+/* I pledge I did not copy or try to copy work from any student.      */
+/* I pledge I did not copy or try to copy work from any where else.   */
+/* I pledge the only person I asked for help from was my teacher.     */
+/* I pledge I did not attempt to help any student on this assignment. */
+/* I understand if I violate this pledge I will receive a 0 grade.    */
+/*                                                                    */
+/*                                                                    */
+/*                      Signed: _____________________________________ */
+/*                                           (signature)              */
+/*                                                                    */
+/*                                                                    */
+/**********************************************************************/
+
+/**********************************************************************/
+/*                                                                    */
+/* This program simulates the environement in UNIX where new          */
+/* processes are continually arriving, existing processes are vying   */
+/* for the CPU, processes are using their given quantum (CPU bound)   */
+/* or blocking because of I/O operations, and processees are          */
+/* terminating when their work is finished.                           */
+/*                                                                    */
+/**********************************************************************/
